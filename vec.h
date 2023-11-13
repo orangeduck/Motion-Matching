ab04eba2a50b724945012d78b4f4adac9892b9cc
Daniel Holden <theonlydancingbanana@hotmail.com>
2023-11-12 19:21:58 -0500

    Update vec.h

--- vec.h
+++ vec.h
@@ -27,7 +27,7 @@ static inline vec2 operator+(vec2 v, vec2 w)
 
 static inline vec2 operator-(float s, vec2 v)
 {
-    return vec2(v.x - s, v.y - s);
+    return vec2(s - v.x, s - v.y);
 }
 
 static inline vec2 operator-(vec2 v, float s)
@@ -122,7 +122,7 @@ static inline vec3 operator+(vec3 v, vec3 w)
 
 static inline vec3 operator-(float s, vec3 v)
 {
-    return vec3(v.x - s, v.y - s, v.z - s);
+    return vec3(s - v.x, s - v.y, s - v.z);
 }
 
 static inline vec3 operator-(vec3 v, float s)
